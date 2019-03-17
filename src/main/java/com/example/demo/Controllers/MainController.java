c0939e57349271acec04ebeb6692165e097f5d31
4eater <git02@latecore.com>
2019-03-17 19:50:04 +0200

    abc

--- src/main/java/com/example/demo/Controllers/MainController.java
+++ src/main/java/com/example/demo/Controllers/MainController.java
@@ -1,7 +1,10 @@
 package com.example.demo.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController
@@ -11,5 +14,32 @@ public class MainController
     {
         return "test";
     }
+    @GetMapping ("/first")
+    public String getFirstSite()
+    {
+        return "first";
+    }
+    @GetMapping ("/second")
+    public String getSecondSite()
+    {
+        return "second";
+    }
+    @PostMapping("/zoho")
+    public String postZoho(
+            @RequestParam String email,
+            @RequestParam String lname,
+            @RequestParam String city,
+            @RequestParam String company,
+            @RequestParam String type,
+            Model model)
+    {
+        model.addAttribute("email", email);
+        model.addAttribute("lname", lname);
+        model.addAttribute("city", city);
+        model.addAttribute("company", company);
+        model.addAttribute("type", type);
+        return "zoho";
+    }
+
 
 }

07b88765940c9745a56500f4480ac38f8275cd03
4eater <git02@latecore.com>
2019-03-17 18:42:39 +0200

    abc

---
+++ src/main/java/com/example/demo/Controllers/MainController.java
@@ -0,0 +1,15 @@
+package com.example.demo.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController
+{
+    @GetMapping ("/test")
+    public String getTestPage()
+    {
+        return "test";
+    }
+
+}
